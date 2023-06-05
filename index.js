8b44a3438c4626e1d2076dbddc5caf16e16d72c2
Milo <74473107+dasmilo@users.noreply.github.com>
2023-06-05 08:43:51 +0200

    change messages & add constants

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// hey.
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client({ });
+const Constants = require("./Constants.js");
+
+client.on('ready', async () => {
+  console.log(`${client.user.username} ist gestartet.`);
+})
+
+client.login(Constants.private.token);
